34a097d474fb3eab8574c33a21462d3bc639a3fd
Oratakashi Nhamako <davidcreazy@gmail.com>
2025-01-11 11:20:07 +0700

    Add Path and Ignore Result

--- home-darwin.nix
+++ home-darwin.nix
@@ -3,6 +3,7 @@
 {
   home.homeDirectory = "/Users/oratakashi";
   home.stateVersion = "23.11"; # Add this line to fix the error
+  home.sessionVariables.PATH = "${config.home.homeDirectory}/.config/home-manager/result/sw/bin:$PATH";
 
   # Tambahkan konfigurasi macOS lainnya
   home.packages = with pkgs; [
@@ -12,6 +13,6 @@
 
   programs.zsh.shellAliases = {
     # ... alias lainnya ...
-    home-update = "./result/sw/bin/darwin-rebuild switch --flake ~/.config/home-manager#oratakashi";
+    home-update = "darwin-rebuild switch --flake ~/.config/home-manager#oratakashi";
   };
 }
